bcd19b846b9551e5f4a1b3aa320733e85fb3e47b
Hoe Kit <hoekit@gmail.com>
2013-07-24 09:35:03 +0700

    Added web.js that support arbitrary port.

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var data = fs.readFileSync('./index.html');
+  var buf = new Buffer(data);
+  response.send(buf.toString());
+});
+
+var port = process.argv[2]|| 8000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
