57193cc165ba0b598d8d2e2cfdde8c1a54c669f5
Hoe Kit <hoekit@gmail.com>
2013-07-25 12:53:45 +0700

    Allows serving files other than index.html.

--- web.js
+++ web.js
@@ -3,13 +3,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var html = process.argv[2] || './index.html';
 app.get('/', function(request, response) {
-  var data = fs.readFileSync('./index.html');
+  var data = fs.readFileSync(html);
   var buf = new Buffer(data);
   response.send(buf.toString());
 });
 
-var port = process.argv[2]|| 8000;
+var port = process.argv[3]|| 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
